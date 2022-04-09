57d2ce50c2b5e9862d9d937a454c4ccb0ed449af
HongInSung <ghddls0704@kakao.com>
2022-04-09 23:30:05 +0900

    디자인 리펙토링중..

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,16 @@
                 <i class="fa-solid fa-xmark" @click="Active"></i>
             </div>
             <div>
-                <p>홍인성 : 안녕하세요! 해당 프로젝트에 총괄역을 맡고있습니다. 전반적인 시스템과 TodoList부분에 디자인 부분을 맡았습니다. 앞으로 더 많은 프로젝트에 만나뵙겠습니다.</p>
-                <p>이민준 : 안녕하세요! 해당 프로젝트에서 메뉴와 Credit창에 디자인을 맡았습니다. ( 사실 실질적인 개발자는 접니ㄷ ) 앞으로도 잘 부탁드리겠습니다.</p>
+                <p>
+                    홍인성 : 안녕하세요! 해당 프로젝트에 총괄역을 맡고있습니다.
+                    전반적인 시스템과 TodoList부분에 디자인 부분을 맡았습니다.
+                    앞으로 더 많은 프로젝트에 만나뵙겠습니다.
+                </p>
+                <p>
+                    이민준 : 안녕하세요! 해당 프로젝트에서 메뉴와 Credit창에
+                    디자인을 맡았습니다. ( 사실 실질적인 개발자는 접니ㄷ )
+                    앞으로도 잘 부탁드리겠습니다.
+                </p>
             </div>
         </div>
     </div>
@@ -22,12 +30,23 @@
     <main>
         <div>
             <div class="main">
+                <img src="./assets/label.png" alt="" class="first" />
                 <div class="sub">
-                    <input type="text" class="addItem" placeholder="추가하고 싶은 Todo를 입력하세요. 엔터키를 누르면 추가됩니다." v-model="newTodoItem" v-on:keyup.enter="addItem" />
-                    <hr />
+                    <div class="container">
+                        <input
+                            type="text"
+                            class="addItem"
+                            v-model="newTodoItem"
+                            v-on:keyup.enter="addItem"
+                        />
+                    </div>
+
                     <div class="items">
-                        <ul v-for="items in todoItems" v-bind:key="items">
-                            <li>{{ items }}</li>
+                        <ul
+                            v-for="(todoItems, index) in items"
+                            v-bind:key="todoItems"
+                        >
+                            <li>{{ items[index].item }}</li>
                             <hr />
                         </ul>
                     </div>
@@ -42,7 +61,7 @@ export default {
     data() {
         return {
             CreditOn: false,
-            newTodoItem: '',
+            newTodoItem: "",
             todoItems: [],
             items: [],
         };
@@ -55,21 +74,23 @@ export default {
             this.CreditOn = false;
         },
         addItem() {
-            if (this.newTodoItem != '') {
+            if (this.newTodoItem != "") {
                 var obj = {
                     complete: false,
                     item: this.newTodoItem,
                 };
                 localStorage.setItem(obj.item, JSON.stringify(obj));
                 this.items.push(obj);
-                this.newTodoItem = '';
+                this.newTodoItem = "";
             }
         },
     },
     created: function () {
         if (localStorage.length > 0) {
             for (var i = 0; i < localStorage.length; i++) {
-                this.items.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+                this.items.push(
+                    JSON.parse(localStorage.getItem(localStorage.key(i)))
+                );
             }
         }
     },
@@ -77,12 +98,14 @@ export default {
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Song+Myung&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Song+Myung&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Noto+Serif+KR&display=swap");
 
 * {
     margin: 0;
-    font-family: 'Ubuntu', sans-serif;
+    font-family: "Ubuntu", sans-serif;
+    scroll-behavior: smooth;
 }
 
 header {
@@ -136,6 +159,7 @@ main {
     position: fixed;
     width: 100%;
     height: 100vh;
+    z-index: 3;
 }
 
 .modal .box {
@@ -157,34 +181,15 @@ main {
     user-select: none;
 }
 
-.main {
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    background-color: #964b00;
-}
-
-.sub {
-    background-color: #fff4a2;
-    width: 95%;
-    height: 95vh;
-    margin-top: 20px;
-}
-
-.sub hr {
-    width: 95%;
-    background-color: black;
-    height: 3px;
-}
-
 .addItem {
-    font-family: 'Song Myung';
-    width: 95%;
-    height: 40px;
-    background: none;
-    color: black;
-    font-size: 20px;
-    font-weight: 600;
+    border: 1px solid rgba(0, 0, 0, 12%);
+}
+.items ul li {
+    font-family: "Noto Serif KR", serif;
+    font-size: 22px;
+}
+
+.items ul hr {
+    width: 100%;
 }
 </style>
