f3e3d4e38339a5973708d5895fe096d09630812c
BackdevHong <ghddls0704@kakao.com>
2022-05-21 23:52:35 +0900

    debug ) 테스트 중

--- hackernews/src/store/mutations.js
+++ hackernews/src/store/mutations.js
@@ -1,23 +1,24 @@
 export default {
-	SET_NEWS(state, news) {
-		state.news = news;
-	},
-	SET_JOBS(state, jobs) {
-		state.jobs = jobs;
-	},
-	SET_ASK(state, ask) {
-		state.ask = ask;
-	},
-	SET_USER(state, user) {
-		state.user = user;
-	},
-	SET_ASKS(state, asks) {
-		state.asks = asks;
-	},
-	SET_LIST(state, list) {
-		state.list = list;
-	},
-	SET_SPIN(state, spin) {
-		state.spin = spin;
-	},
+    SET_NEWS(state, news) {
+        state.news = news;
+    },
+    SET_JOBS(state, jobs) {
+        state.jobs = jobs;
+    },
+    SET_ASK(state, ask) {
+        state.ask = ask;
+    },
+    SET_USER(state, user) {
+        state.user = user;
+    },
+    SET_ASKS(state, asks) {
+        state.asks = asks;
+    },
+    SET_LIST(state, list) {
+        state.list = list;
+    },
+    SET_SPIN(state, spin) {
+        state.spin = spin;
+        console.log(spin);
+    },
 };
