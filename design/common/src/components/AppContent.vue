46a858ea1e5d4937f19f071eff5fd4576f79deb1
HongInSung <ghddls0704@kakao.com>
2022-05-23 16:51:57 +0900

    컴포넌트 디자인 패턴

---
+++ design/common/src/components/AppContent.vue
@@ -0,0 +1,21 @@
+<template>
+	<ul>
+		<li v-for="item in items" :key="item">
+			{{ item }}
+		</li>
+	</ul>
+	<button @click="$emit('renew')">renew items</button>
+</template>
+
+<script>
+export default {
+	props: {
+		items: {
+			type: Array,
+			required: true,
+		},
+	},
+};
+</script>
+
+<style></style>
