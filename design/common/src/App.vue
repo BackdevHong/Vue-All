6a1195bf4300527af1ba0898efa742b346439a1b
HongInSung <ghddls0704@kakao.com>
2022-05-23 16:56:25 +0900

    edit: Common Edit, deleted Item.vue

--- design/common/src/App.vue
+++ design/common/src/App.vue
@@ -1,9 +1,28 @@
 <template>
-	<div></div>
+	<div>
+		<app-header :title="appTitle"></app-header>
+		<app-content :items="items" @renew="renewItems"></app-content>
+	</div>
 </template>
 
 <script>
-export default {};
+import AppHeader from "./components/AppHeader.vue";
+import AppContent from "./components/AppContent.vue";
+export default {
+	components: {
+		AppHeader,
+		AppContent,
+	},
+	data() {
+		return {
+			appTitle: "Common Approach",
+			items: [10, 20, 30],
+		};
+	},
+	methods: {
+		renewItems() {
+			this.items = [40, 50, 60];
+		},
+	},
+};
 </script>
-
-<style></style>
