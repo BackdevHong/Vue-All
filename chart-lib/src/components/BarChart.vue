31018f91e3fe8f28890ff0bbaeb4337f0541dc2e
HongInSung <ghddls0704@kakao.com>
2022-05-23 15:39:33 +0900

    chartlib

---
+++ chart-lib/src/components/BarChart.vue
@@ -0,0 +1,35 @@
+<template>
+	<canvas id="myChart" ref="BarChart" width="50" height="50"></canvas>
+</template>
+
+<script>
+export default {
+	props: ["propsdata"],
+	mounted() {
+		// var ctx = document.getElementById("myChart");
+		const ctx = this.$refs.BarChart;
+		var myChart = new (ctx,
+		{
+			type: "bar",
+			data: {
+				labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+				datasets: this.propsdata,
+			},
+			options: {
+				scales: {
+					yAxes: [
+						{
+							ticks: {
+								beginAtZero: true,
+							},
+						},
+					],
+				},
+			},
+		})();
+		console.log(myChart);
+	},
+};
+</script>
+
+<style></style>
---
+++ chart-lib/src/components/LineChart.vue
@@ -0,0 +1,37 @@
+<template>
+	<canvas id="myChart2" ref="LineChart"></canvas>
+</template>
+
+<script>
+export default {
+	mounted() {
+		// const ctx = document.getElementById("myChart2").getContext("2d");
+		const ctx = this.$refs.LineChart;
+		const myChart = new this.$_Chart(ctx, {
+			type: "line",
+			data: {
+				labels: [
+					"January",
+					"February",
+					"March",
+					"April",
+					"May",
+					"June",
+				],
+				datasets: [
+					{
+						label: "My First dataset",
+						backgroundColor: "rgb(255, 99, 132)",
+						borderColor: "rgb(255, 99, 132)",
+						data: [0, 10, 5, 2, 20, 30, 45],
+					},
+				],
+				borderWidth: 1,
+			},
+		});
+		console.log(myChart);
+	},
+};
+</script>
+
+<style></style>
