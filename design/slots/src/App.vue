214d04f3bc9daa2a342b3f45da7cfcea778a68e7
HongInSung <ghddls0704@kakao.com>
2022-05-24 09:58:37 +0900

    slot에 여러가지 사용점!

--- design/slots/src/App.vue
+++ design/slots/src/App.vue
@@ -2,10 +2,14 @@
 	<div>
 		<ul>
 			<item>아이템 1</item>
-			<item>아이템 2</item>
-			<item>아이템 3</item>
-			<item>아이템 4</item>
-			<item>아이템 5</item>
+			<item>아이템 2<button>click</button></item>
+			<item>
+				<div>아이템 3</div>
+				<img src="./assets/logo.png" alt="" />
+			</item>
+			<item>
+				<div style="color: blue; font-size: 20px">아이템 4</div>
+			</item>
 		</ul>
 	</div>
 </template>
