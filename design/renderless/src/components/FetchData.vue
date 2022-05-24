0b3a578648f79e24a562b327bc238b8b040d50df
HongInSung <ghddls0704@kakao.com>
2022-05-24 16:59:58 +0900

    renderless 구현중

---
+++ design/renderless/src/components/FetchData.vue
@@ -0,0 +1,31 @@
+<script>
+import axios from "axios";
+
+export default {
+	props: ["url"],
+	data() {
+		return {
+			response: null,
+			loading: false,
+		};
+	},
+	created() {
+		axios
+			.get(this.url)
+			.then((response) => {
+				this.response = response.data;
+				this.loading = true;
+			})
+			.catch((error) => {
+				alert("[Error] fetching the data", error);
+				console.log(error);
+			});
+	},
+	render() {
+		return this.$slots.default({
+			response: this.response,
+			loading: this.loading,
+		});
+	},
+};
+</script>
