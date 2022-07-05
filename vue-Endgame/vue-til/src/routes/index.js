55dcb5e2121d7c30555c1987f04b83feb6129f04
HongInSung <ghddls0704@kakao.com>
2022-07-05 12:12:14 +0900

    feat: Add Components

--- vue-Endgame/vue-til/src/routes/index.js
+++ vue-Endgame/vue-til/src/routes/index.js
@@ -2,12 +2,20 @@ import { createWebHistory, createRouter } from "vue-router";
 
 const routes = [
 	{
+		path: "/",
+		redirect: "/login",
+	},
+	{
 		path: "/login",
 		component: () => import("@/views/LoginPage.vue"),
 	},
 	{
 		path: "/signup",
 		component: () => import("@/views/SignupPage.vue"),
+	},
+	{
+		path: "/:catchAll(.*)",
+		component: () => import("@/views/NotFoundPage.vue"),
 	},
 ];
 
