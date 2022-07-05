2126d1046b15a2b6bdfd447c585a256096ea0e8d
BackdevHong <ghddls0704@kakao.com>
2022-07-05 23:17:19 +0900

    feat: Add Register User System

--- vue-Endgame/vue-til/src/components/SignupForm.vue
+++ vue-Endgame/vue-til/src/components/SignupForm.vue
@@ -1,9 +1,43 @@
 <template>
-	<div>signup</div>
+	<form @submit.prevent="submitForm">
+		<div>
+			<label for="username">아이디: </label>
+			<input type="text" id="username" v-model="username" />
+		</div>
+		<div>
+			<label for="password">비밀번호: </label>
+			<input type="text" id="password" v-model="password" />
+		</div>
+		<div>
+			<label for="nickname">닉네임: </label>
+			<input type="text" id="nickname" v-model="nickname" />
+		</div>
+		<button type="submit">login</button>
+	</form>
 </template>
 
 <script>
-export default {};
+import { registerUser } from "@/api/index";
+
+export default {
+	data() {
+		return {
+			username: "",
+			password: "",
+			nickname: "",
+		};
+	},
+	methods: {
+		submitForm() {
+			const userData = {
+				username: this.username,
+				password: this.password,
+				nickname: this.nickname,
+			};
+			registerUser(userData);
+		},
+	},
+};
 </script>
 
 <style lang="scss" scoped></style>
