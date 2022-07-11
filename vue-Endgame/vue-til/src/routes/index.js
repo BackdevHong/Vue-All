27aafe051c405ee5b5cf8fdd9e52a2e8aa54d502
HongInSung <ghddls0704@kakao.com>
2022-07-11 14:31:38 +0900

    feat: Add VueX

--- vue-Endgame/vue-til/src/routes/index.js
+++ vue-Endgame/vue-til/src/routes/index.js
@@ -14,6 +14,10 @@ const routes = [
 		component: () => import("@/views/SignupPage.vue"),
 	},
 	{
+		path: "/main",
+		component: () => import("@/views/MainPage.vue"),
+	},
+	{
 		path: "/:catchAll(.*)",
 		component: () => import("@/views/NotFoundPage.vue"),
 	},
